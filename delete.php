a02bdce3d3b46edfe82d9ed3aeab169ed21215bd
unknown <priyankapandey12881@gmail.com>
2023-07-15 07:32:53 +0530

    First commit

---
+++ delete.php
@@ -0,0 +1,16 @@
+<?php
+if (isset($_GET['delete'])) {
+    $hotelId = $_GET['delete'];
+    
+    
+    $deleteQuery = "DELETE FROM hotels WHERE id = '$hotelId'";
+    mysqli_query($con, $deleteQuery);
+    
+
+    $deleteImagesQuery = "DELETE FROM images WHERE hotel_id = '$hotelId'";
+    mysqli_query($con, $deleteImagesQuery);
+    
+    header("Location: list.php");
+    exit();
+}
+?>
